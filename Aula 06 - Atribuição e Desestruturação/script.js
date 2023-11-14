6b1ecca575a2ac563f383d08b7c7fb643fce9c9e
Eduardo Secco <dudssecco.code@gmail.com>
2023-11-14 11:56:03 -0300

    Update script.js

--- Aula 06 - Atribuição e Desestruturação/script.js
+++ Aula 06 - Atribuição e Desestruturação/script.js
@@ -7,4 +7,15 @@ function MeuEscopo(){
 
 }
 
+function MeuEscopo2(){
+
+//                        0         1         2
+//                     0  1  2   0  1  2   0  1  2
+    const numeros2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    const [lista1, lista2, lista3] = numeros2;
+    console.log(lista3[2]);
+
+}
+
 MeuEscopo();
+MeuEscopo2();
