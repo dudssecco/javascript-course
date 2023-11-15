5890e03cb982a6dad612ab1441536250d2160a4a
Eduardo Secco <dudssecco.code@gmail.com>
2023-11-15 20:38:42 -0300

    Create script.js

---
+++ Aula 08 - For/script.js
@@ -0,0 +1,16 @@
+function MeuEscopo(){
+
+    for (let i = 0; i <=5; i++){
+        console.log(`Linha ${i}`)
+    }
+
+    //                0       1       2
+    const frutas = ['Maça', 'Perâ', 'Uva']
+
+    for (let t = 0; t < frutas.length; t++){
+        console.log(`Indice ${t}`, frutas[t])
+    }
+
+}
+
+MeuEscopo();
