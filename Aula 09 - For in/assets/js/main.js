03d1df99e40fe7e4151eeb11cc154dcfc64e9443
Eduardo Secco <dudssecco.code@gmail.com>
2023-11-16 09:36:24 -0300

    Update main.js

---
+++ Aula 09 - For in/assets/js/main.js
@@ -0,0 +1,16 @@
+const frutas = ['Maça', 'Uva', 'Melancia']
+
+for (let i in frutas){
+    console.log(frutas[i])
+}
+
+const pessoas = {
+    nome: 'Duds',
+    sobrenome: 'Secco',
+    idade: 21,
+    cidade: 'Floripa'
+}
+
+for (let chave in pessoas){
+    console.log(chave, pessoas[chave])
+}
