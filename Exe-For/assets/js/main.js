5d95c3b41cff72a27c3d984f13b8bd27d0e6ce50
Eduardo Secco <dudssecco.code@gmail.com>
2023-11-15 22:52:43 -0300

    Create Exe

---
+++ Exe-For/assets/js/main.js
@@ -0,0 +1,18 @@
+const elementos = [
+    {tag: 'p', texto: 'Texto 1'},
+    {tag: 'div', texto: 'Texto 2'},
+    {tag: 'section', texto: 'Texto 3'},
+    {tag: 'footer', texto: 'Texto 4'},
+]
+
+const container = document.querySelector('.container');
+const div = document.createElement('div')
+
+for (let i = 0; i < elementos.length; i++){
+    let {tag, texto} = elementos[i]
+    let tagHTML = document.createElement(tag)
+    tagHTML.innerHTML = texto
+    div.appendChild(tagHTML)
+}
+
+container.appendChild(div)
