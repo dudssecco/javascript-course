4d0301f5f87defbe33fdb0d5e7ff84fb1b073300
Eduardo Secco <dudssecco.code@gmail.com>
2023-11-24 11:21:55 -0300

    Update script.js

--- Aula 25 - Concatenação de Arrays/script.js
+++ Aula 25 - Concatenação de Arrays/script.js
@@ -3,5 +3,6 @@ const array2 = [4, 5, 6]
 
 const array3 = array1.concat(array2, [7, 8, 9], 'Duds')
 const array4 = [...array1, ...array2, ...[7, 8, 9], 'Duds']
+
 console.log(array3)
 console.log(array4)
