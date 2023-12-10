5c33ab1b0458e41f0b4343921e1a78a98103ee67
Eduardo Secco <dudssecco.code@gmail.com>
2023-12-09 22:30:39 -0300

    Create script.js

---
+++ Aula 38 - Factory + Prototype/script.js
@@ -0,0 +1,13 @@
+function criaPessoa(nome, sobrenome){
+
+    const pessoaPrototype = {
+        
+    }
+
+    return{
+        nome,
+        sobrenome
+    }
+}
+
+const p1 = criaPessoa('Eduardo', 'Secco')
