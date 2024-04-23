d92a8a1074d0264173f4430a9873faca024686cc
Eduardo Secco <dudssecco.code@gmail.com>
2024-04-23 09:16:34 -0300

    Update script.js

--- Aula 46 - Async & Await/script.js
+++ Aula 46 - Async & Await/script.js
@@ -35,11 +35,11 @@ async function executa(){
 
     const fase1 = await esperaAi('Fase 1', numAleatorio())
     console.log(fase1)
-    const fase2 = await esperaAi(2, numAleatorio())
+    const fase2 = await esperaAi('Fase 2', numAleatorio())
     console.log(fase2)
     const fase3 = await esperaAi('Fase 3', numAleatorio())
     console.log(fase3)
-    console.log('Terminamos na frase', fase3)
+    console.log('Terminamos na fase', fase3)
 
     } catch(e){
         console.log(e)
