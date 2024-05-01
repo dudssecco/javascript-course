8b513e0b7a334fe0886fe05f6f0b83989141db89
Eduardo Secco <dudssecco.code@gmail.com>
2024-05-01 20:13:53 -0300

    Update

---
+++ Content/Aula 52 - Import Export/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var duds = 2;
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
