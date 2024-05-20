e039702d8737d3c69056595500e8b03d28f35878
Eduardo Secco <dudssecco.code@gmail.com>
2024-05-20 15:47:14 -0300

    Up

--- Content-Node/Aula 17 - Helmet e CSRF/server.js
+++ Content-Node/Aula 17 - Helmet e CSRF/server.js
@@ -5,26 +5,26 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.emit('pronto');
+    app.emit('pronto'); // Quando for conectado vai emitir um sinal "pronto".
   })
   .catch(e => console.log(e));
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
-const routes = require('./routes');
-const path = require('path');
-const helmet = require('helmet');
-const csrf = require('csurf');
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+const session = require('express-session'); // Seções criadas para salvar um cookie no navegador do cliente para toda vez que ele logar no servidor esse cookie ser checkado.
+const MongoStore = require('connect-mongo'); // Seções vão ser salvas dentro da base de dados.
+const flash = require('connect-flash'); // Mensagens "Auto-Destrutivas" (Mensagens salvas em seções).
+const routes = require('./routes'); // Rotas da nossa API .
+const path = require('path'); // Caminhos.
+const helmet = require('helmet'); // Recomendação do express.
+const csrf = require('csurf'); // Tokens criados para nossos formulários para a segurança.
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware'); // Middlewares são funções executadas nas rotas.
 
 app.use(helmet());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true })); // Pode postar formulários para dentro da API
+app.use(express.json()); // Pode fazer o parse de .json para dentro da API
+app.use(express.static(path.resolve(__dirname, 'public'))); // Arquivos estaticos que devem ser acessados diretamente (Imagens, textos, css, js...)
 
-const sessionOptions = session({
+const sessionOptions = session({  // Configurações de seção
   secret: 'akasdfj0út23453456+54qt23qv',
   store: MongoStore.create({ mongoUrl: process.env.CONNECTION }),
   resave: false,
@@ -38,8 +38,8 @@ const sessionOptions = session({
 app.use(sessionOptions);
 app.use(flash());
 
-app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'src', 'views')); // Arquivos que visualizamos/renderizamos na tela (arquivos.html) 
+app.set('view engine', 'ejs'); // Engine que estamos utilizando para renderizar 
 
 app.use(csrf());
 
@@ -49,7 +49,7 @@ app.use(checkCsrfError);
 app.use(csrfMiddleware);
 app.use(routes);
 
-app.on('pronto', () => {
+app.on('pronto', () => {  // Recebe o sinal pronto e imprime no console a URL e a Porta.
   app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
     console.log('Servidor executando na porta 3000');
